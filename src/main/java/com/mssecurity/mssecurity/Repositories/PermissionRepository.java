030c90fba3364a5685559fbdea9f7599c99c29eb
Liceth02usma <juanitausma02@gmail.com>
2023-09-29 22:47:05 -0500

    updata repositories

---
+++ src/main/java/com/mssecurity/mssecurity/Repositories/PermissionRepository.java
@@ -0,0 +1,14 @@
+package com.mssecurity.mssecurity.Repositories;
+
+import com.mssecurity.mssecurity.Models.Permission;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface PermissionRepository extends MongoRepository<Permission,String> {
+    @Query("{'url':?0,'method':?1}")
+    Permission getPermission(String url, String method);
+}
+
+
+
+/// nota cambiar todas las urls otra vez, o mejor crear eso de nuevo.
---
+++ src/main/java/com/mssecurity/mssecurity/Repositories/RolePermissionRepository.java
@@ -0,0 +1,10 @@
+package com.mssecurity.mssecurity.Repositories;
+
+import com.mssecurity.mssecurity.Models.RolePermission;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface RolePermissionRepository extends MongoRepository<RolePermission,String> {
+    @Query("{'role.$id': ObjectId(?0),'permission.$id': ObjectId(?1)}")
+    RolePermission getRolePermission(String roleId,String permissionId);
+}
