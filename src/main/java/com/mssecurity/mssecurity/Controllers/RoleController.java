d9d708c46c37ad15a7e83ee5dffb1491be8d2575
Liceth02usma <juanitausma02@gmail.com>
2023-09-30 19:00:47 -0500

    Correcion de error

---
+++ src/main/java/com/mssecurity/mssecurity/Controllers/RoleController.java
@@ -0,0 +1,28 @@
+package com.mssecurity.mssecurity.Controllers;
+
+import com.mssecurity.mssecurity.Models.Role;
+import com.mssecurity.mssecurity.Repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/roles")
+public class RoleController {
+    @Autowired
+    private RoleRepository theRoleRepository;
+
+    @GetMapping("")
+    public List<Role> index() {
+        return this.theRoleRepository.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Role store(@RequestBody Role newRole) {
+        return this.theRoleRepository.save(newRole);
+    }
+}
